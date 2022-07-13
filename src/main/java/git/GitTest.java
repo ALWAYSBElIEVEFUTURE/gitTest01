8a8cd7f6a074796839c774e036a3eabeb2981d19
maguoqing <3547123360@qq.com>
2022-07-13 15:37:01 +0800

    third commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
