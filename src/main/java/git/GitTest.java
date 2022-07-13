6908ebd16b74cf1e955287858ecf729b41cc9313
maguoqing <3547123360@qq.com>
2022-07-13 15:58:56 +0800

    hot-fix second commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
